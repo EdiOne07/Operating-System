4cbdd02ad3272714e305487031349c249af0dcba
EdiOne07 <edione2002@gmail.com>
2023-05-10 22:02:25 +0300

    "Added process, menu now works perfectly"

--- Project/Project_OS.c
+++ Project/Project_OS.c
@@ -5,6 +5,8 @@
 #include <dirent.h>
 #include <time.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 void access_rights(struct stat file){
     printf("User:\n");
     if(file.st_mode & S_IRUSR){
@@ -97,78 +99,102 @@ void count_c_files(char* name){
 }
 int main(int argc, char *argv[]){
     struct stat a;
+    pid_t process;
     char c;
-    for(int i=0;i<argc-1;i++){
-        if(lstat(argv[i+1],&a)==-1){
-            perror(argv[i+1]);
+    if(argc<=1){
+        printf("Invalid input");
+        exit(0);
+    }
+    for(int i=1;i<argc;i++){
+        if(lstat(argv[i],&a)==-1){
+            perror(argv[i]);
             continue;
         }
+            process=fork();
+            if(process<0){
+                perror("Process didn't start");
+            }else if(process==0){
         if(S_ISREG(a.st_mode)==1){
             struct stat file;
             char link_name[32];
             lstat(argv[i+1],&file);
             struct tm* timer=localtime(&file.st_mtime);
-            printf("%s is a regular file\n", argv[i+1]);
+            printf("%s is a regular file\n", argv[i]);
             printf("Regular file:\n name(-n)\n size(-d)\n hard link count(-h)\n time of last modification(-m)\n access rights(-a)\n create symbolic link(-l)\n");
-            scanf("%c", &c);
+            scanf(" %c", &c);
             if(c=='-'){
-                scanf("%c", &c);
+                scanf(" %c", &c);
             }
             switch(c){
-                case 'n': printf("The name of the file is %s",argv[i+1]); break;
-                case 'd': printf("The size of the file is %ld",file.st_size); break;
-                case 'h': printf("The hard link count is %ld",file.st_nlink); break;
-                case 'm': printf("The time of the last modification is %ld",file.st_mtime);break;
+                case 'n': printf("The name of the file is %s\n",argv[i]); break;
+                case 'd': printf("The size of the file is %ld\n",file.st_size); break;
+                case 'h': printf("The hard link count is %ld\n",file.st_nlink);break;
+                case 'm': printf("The time of the last modification is %ld\n",file.st_mtime);break;
                 case 'a': printf("The access rights are:\n");
                           access_rights(file);
                           break;
                 case 'l': printf("Input the file name for the symbolic link\n");
                           scanf("%s",link_name);
-                          symlink(argv[i+1],link_name);
+                          symlink(argv[i],link_name);
                           printf("The symbolic link %s is created",link_name);
                           break;
-                default:fflush(stdin);printf("Not a regular file\n");break;
+                default:printf("Not a regular file\n");break;
             }
-        }
+          }
         else if(S_ISLNK(a.st_mode)==1){
             struct stat file;
-            lstat(argv[i+1],&file);
-            printf("%s is a symbolic link file\n",argv[i+1]);
+            lstat(argv[i],&file);
+            printf("%s is a symbolic link file\n",argv[i]);
             printf("Symbolic link:\n name(-n)\n delete symbolic link(-l)\n size of symbolic link(-d)\n size of target file(-t)\n access rights(-a)\n");
             scanf("%c",&c);
             if(c=='-'){
                 scanf("%c",&c);
             }
             switch(c){
-                case 'n': link_file_name(argv[i+1]); break;
-                case 'l': unlink(argv[i+1]);
+                case 'n': link_file_name(argv[i]); break;
+                case 'l': unlink(argv[i]);
                           printf("The link was deleted\n");break;
                 case 'd': printf("The size of the link file is %ld\n",file.st_size);break;
-                case 't': link_file_size(argv[i+1]);break;
+                case 't': link_file_size(argv[i]);break;
                 case 'a': access_rights(file);break;
                 default:printf("Not a symbolic link file\n");break;
             }
         }
         else if(S_ISDIR(a.st_mode)==1){
             struct stat file;
-            lstat(argv[i+1],&file);
-            printf("%s is a directory\n",argv[i+1]);
+            lstat(argv[i],&file);
+            printf("%s is a directory\n",argv[i]);
             printf("Directory:\n name(-n)\n size(-d)\n access rights(-a)\n total number of file with the .c extension(-c)\n");
             scanf("%c",&c);
             if(c=='-'){
                 scanf("%c",&c);
             }
             switch(c){
-                case 'n': printf("The directory name is %s:\n",argv[i+1]);break;
+                case 'n': printf("The directory name is %s:\n",argv[i]);break;
                 case 'd': printf("Size of directory is %ld:\n",file.st_size);break;
                 case 'a': access_rights(file);break;
-                case 'c': count_c_files(argv[i+1]);break;
+                case 'c': count_c_files(argv[i]);break;
                 default:printf("Not a directory\n");break;
             }
         }
         else{
             printf("File given as argument is not a good input\n");
         }
+        exit(0);
     }
+    /*process=fork();
+    if(process<0){
+        perror("Couldn't open process");
+        exit(1);
+    }
+    else if(process==0){
+        //if(.c file)
+            execlp("./script name","./script name",argv[i],NULL);
+    }*/
+    else{
+        int status;
+        wait(&status);
+    }
+  }
    return 0;
 }
